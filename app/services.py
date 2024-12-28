c8e3d9751803d21302971879214cb44a09f5968a
Priyanka Ippala <priyankaippala2003@gmail.com>
2024-12-28 16:28:47 +0530

    added pdf extraction

--- app/services.py
+++ app/services.py
@@ -9,7 +9,7 @@ def generate_interview_questions(resume_summary, job_description):
     """
     try:
         # Format the prompt
-        prompt = f"Generate interview questions based on the following resume and job description: Resume Summary: {resume_summary} Job Description: {job_description} Questions: in text format"
+        prompt = f"Generate interview questions based on the following resume and job description: Job Description: {job_description} Questions: in text format"
         
         # Use Gemini API to generate questions
         model = genai.GenerativeModel("gemini-1.5-flash")
