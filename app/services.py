7181a12bfdb9db63dee01d36ef73fba841b496c3
Priyankaippala <114237847+Priyankaippala@users.noreply.github.com>
2025-02-04 10:07:47 +0530

    Update services.py

--- app/services.py
+++ app/services.py
@@ -8,7 +8,7 @@ def generate_interview_questions(resume_summary, job_description):
     Generates interview questions using the Gemini API.
     """
     try:
-        prompt = f"Generate interview questions based on the following resume and job description: resume : {resume_summary} Job Description: {job_description} Questions: JSON-like structure to represent the sections, subsections, and questions.Strictly stick to this format without any extra data/text in the response."
+        prompt = f"Generate interview 10 questions based on the following resume and job description: resume : {resume_summary} Job Description: {job_description} Questions: JSON-like structure to represent the sections, subsections, and questions.Strictly stick to this format without any extra data/text in the response."
         
         # Use Gemini API to generate questions
         model = genai.GenerativeModel("gemini-1.5-flash")
